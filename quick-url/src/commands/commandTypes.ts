1b197bd5b15bf1f036e0fa51ca84c7533bddd6a7
rubengardner <rgardnercuesta@gmail.com>
2024-08-27 16:30:53 -0600

    Refactor command type

--- quick-url/src/commands/commandTypes.ts
+++ quick-url/src/commands/commandTypes.ts
@@ -1,8 +1,18 @@
-export interface Command {
+export interface BaseCommand {
     id: string;
     name: string;
-    url: string;
     shortcut: string;
     type: string;
 }
 
+export interface NavigateCommand extends BaseCommand {
+    type: 'navigate';
+    url: string;
+}
+
+export interface CopyCommand extends BaseCommand {
+    type: 'copy';
+    textToCopy: string;
+}
+
+export type Command = NavigateCommand | CopyCommand;
