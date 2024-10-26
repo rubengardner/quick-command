e8d95676b6af92cf07890ad094cda22ecf6b031a
ruben-gardner <ruben.gardner@badgermapping.com>
2024-10-26 17:28:25 +0200

    Add global files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Menu from './components/Menu/Menu';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
         <Menu/>
     </div>
   );
