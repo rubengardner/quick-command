4f7804cc2e73c74ee783a278425777ddb4c5420b
rubengardner <rgardnercuesta@gmail.com>
2024-10-27 16:43:03 +0100

    Update the save hook to reflect the uuid command

--- src/dataAccess/useSaveCommands.ts
+++ src/dataAccess/useSaveCommands.ts
@@ -1,24 +1,35 @@
 import {Command} from "../commands/commandTypes";
+import {v4 as uuidv4} from 'uuid';
 
 const useSaveCommands = () => {
-  return (newCommand: Partial<Command>) => {
-    // @ts-ignore
-    chrome.storage.sync.get(['commands']).then((result) => {
-      const existingCommands = result.commands || [];
+    return (newCommand: Partial<Command>) => {
+        // @ts-ignore
+        chrome.storage.sync.get(['commands']).then((result) => {
+            const existingCommands = result.commands || [];
 
-      const updatedCommands = [...existingCommands, newCommand];
+            const commandIndex = existingCommands.findIndex((cmd: Command) => cmd.id === newCommand.id);
 
-      // @ts-ignore
-      chrome.storage.sync.set({commands: updatedCommands}).then(() => {
-        console.log('Commands saved successfully!');
-      }).catch((err: any) => {
-        console.error('Error saving commands:', err);
-      });
+            let updatedCommands;
+            if (commandIndex !== -1) {
+                updatedCommands = [
+                    ...existingCommands.slice(0, commandIndex),
+                    {...existingCommands[commandIndex], ...newCommand},
+                    ...existingCommands.slice(commandIndex + 1)
+                ];
+            } else {
+                updatedCommands = [...existingCommands, {...newCommand, id: newCommand.id || uuidv4()}];
+            }
+            // @ts-ignore
+            chrome.storage.sync.set({commands: updatedCommands}).then(() => {
+                console.log('Command saved successfully!');
+            }).catch((err: any) => {
+                console.error('Error saving commands:', err);
+            });
 
-    }).catch((err: any) => {
-      console.error('Error fetching commands:', err);
-    });
-  };
+        }).catch((err: any) => {
+            console.error('Error fetching commands:', err);
+        });
+    };
 };
 
 export default useSaveCommands;
