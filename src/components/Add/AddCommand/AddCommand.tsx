b0830cc5f44c519a566d57938cdb9d6704ec3293
rubengardner <rgardnercuesta@gmail.com>
2024-10-27 14:50:32 +0100

    Add animation logic

--- src/components/Add/AddCommand/AddCommand.tsx
+++ src/components/Add/AddCommand/AddCommand.tsx
@@ -1,19 +1,26 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {Command, CommandType} from '../../../commands/commandTypes';
 import styles from './AddCommand.module.css';
 import TypeSpecificFields from "../TypeSpecificFields/TypeSpecificFields";
 import useSaveCommands from "../../../dataAccess/useSaveCommands";
 
-const AddCommand = () => {
+
+interface AddCommandProps {
+    setActiveTab: React.Dispatch<React.SetStateAction<'terminal' | 'commands' | 'add'>>;
+}
+
+const AddCommand = (props: AddCommandProps) => {
+    const {setActiveTab} = props;
     const saveCommands = useSaveCommands();
     const [newCommand, setNewCommand] = useState<Partial<Command>>({
         name: "",
         shortcut: "",
         type: CommandType.Navigate,
     });
+    const [isAnimating, setIsAnimating] = useState(false);
 
     const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        const { value } = e.target;
+        const {value} = e.target;
         setNewCommand((prev) => ({
             ...prev,
             type: value as CommandType
@@ -21,7 +28,7 @@ const AddCommand = () => {
     };
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
+        const {name, value} = e.target;
         setNewCommand((prev) => ({
             ...prev,
             [name]: value,
@@ -29,13 +36,21 @@ const AddCommand = () => {
     };
 
     const handleSubmit = () => {
+        setIsAnimating(true);
         saveCommands(newCommand);
+
+        setTimeout(() => {
+            setIsAnimating(false);
+            setActiveTab('terminal');
+        }, 500);
     };
 
     const commandValues = Object.values(CommandType);
 
     return (
         <div className={styles.container}>
+            {isAnimating && <div className={styles.animationBackground}></div>}
+
             <h2 className={styles.title}>Add Command</h2>
             <label className={styles.label}>
                 Name:
@@ -76,7 +91,7 @@ const AddCommand = () => {
                 commandData={newCommand}
                 onFieldChange={handleInputChange}
             />
-            <button className={styles.saveButton} onClick={handleSubmit}>
+            <button className={styles.saveButton} onClick={handleSubmit} disabled={isAnimating}>
                 Add Command
             </button>
         </div>
