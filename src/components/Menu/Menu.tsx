37e77b57ce8a6d64b0b8d1132d9d9b203e45ad8e
rubengardner <rgardnercuesta@gmail.com>
2024-10-27 17:33:14 +0100

    Update menu to containin the save logic

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,14 +1,34 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import styles from './Menu.module.css';
 import Terminal from '../Terminal/Terminal';
 import CommandDashboard from "../CommandDashboard/CommandDashboard";
 import AddCommand from "../Add/AddCommand/AddCommand";
+import useSaveCommands from "../../dataAccess/useSaveCommands";
+import {Command} from "../../commands/commandTypes";
 import useGetCommands from "../../dataAccess/useGetCommands";
 
-
 const Menu = () => {
-    const commands = useGetCommands();
+    const [commands, setCommands] = useState<Command[]>([]);
     const [activeTab, setActiveTab] = useState<'terminal' | 'commands' | 'add'>('terminal');
+    const saveCommands = useSaveCommands();
+    const fetchCommands = useGetCommands();
+
+    useEffect(() => {
+        const fetchAllCommands = async () => {
+            const allCommands = await fetchCommands();
+            setCommands(allCommands);
+        };
+
+        fetchAllCommands();
+    }, [fetchCommands]);
+
+
+    const handleSave = async (newCommand: Partial<Command>) => {
+        await saveCommands(newCommand);
+        const allCommands = await fetchCommands();
+        setCommands(allCommands);
+        setActiveTab('commands');
+    };
 
     return (
         <div className={styles.menuContainer}>
@@ -34,7 +54,7 @@ const Menu = () => {
             </div>
             <div className={styles.content}>
                 {activeTab === 'terminal' && <Terminal commands={commands}/>}
-                {activeTab === 'commands' && <CommandDashboard commands={commands}/>}
+                {activeTab === 'commands' && <CommandDashboard commands={commands} handleSave={handleSave}/>}
                 {activeTab === 'add' && <AddCommand setActiveTab={setActiveTab}/>}
             </div>
         </div>
